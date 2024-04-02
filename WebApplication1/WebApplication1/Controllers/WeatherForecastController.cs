bd8a95ba2b57c7ba1a8795ca8ab489b4d002a129
ELVINA0 <elvinahazieva685@gmail.com>
2024-04-02 23:25:08 +0300

    обновление кода(Возврат отсортированной обработанной строки)

--- WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace StringManipulationApp.Controllers
 {
@@ -7,15 +10,41 @@ namespace StringManipulationApp.Controllers
     public class StringManipulationController : ControllerBase
     {
         [HttpPost]
-        public ActionResult<string> ProcessString([FromBody] string inputString)
+        public ActionResult<object> ProcessString([FromBody] string inputString, [FromQuery] string sortAlgorithm)
         {
             if (string.IsNullOrEmpty(inputString))
             {
                 return BadRequest("Input string cannot be empty");
             }
 
-            string processedString = string.Empty;
+            // Check if the input string contains only lowercase English letters
+            string invalidCharacters = new string(inputString.Where(c => !Char.IsLetter(c) || !Char.IsLower(c) || c < 'a' || c > 'z').ToArray());
+            if (!string.IsNullOrEmpty(invalidCharacters))
+            {
+                return BadRequest($"Invalid character(s) detected: {invalidCharacters}");
+            }
 
+            string processedString = string.Empty;
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+            List<string> substrings = new List<string>();
+
+            foreach (char c in inputString)
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount[c] = 1;
+                }
+            }
+
+            string vowels = "aeiouy";
+            int maxVowelSubstringLength = 0;
+            string maxVowelSubstring = "";
+
+            // Process inputString
             if (inputString.Length % 2 == 0)
             {
                 int middleIndex = inputString.Length / 2;
@@ -29,7 +58,6 @@ namespace StringManipulationApp.Controllers
                 char[] reversedSecondHalf = secondHalf.ToCharArray();
                 Array.Reverse(reversedSecondHalf);
                 string reversedSecondHalfStr = new string(reversedSecondHalf);
-
                 processedString = reversedFirstHalfStr + reversedSecondHalfStr;
             }
             else
@@ -37,11 +65,57 @@ namespace StringManipulationApp.Controllers
                 char[] charArray = inputString.ToCharArray();
                 Array.Reverse(charArray);
                 string reversedString = new string(charArray);
-
                 processedString = reversedString + inputString;
             }
 
-            return Ok(processedString);
+            // Find the longest substring starting and ending with a vowel in processedString
+            for (int i = 0; i < processedString.Length; i++)
+            {
+                if (vowels.Contains(processedString[i]))
+                {
+                    for (int j = i + 1; j < processedString.Length; j++)
+                    {
+                        if (vowels.Contains(processedString[j]))
+                        {
+                            string substring = processedString.Substring(i, j - i + 1);
+                            if (substring.Length > maxVowelSubstringLength)
+                            {
+                                maxVowelSubstringLength = substring.Length;
+                                maxVowelSubstring = substring;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Sort 
+            string sortedString = string.Empty;
+            if (sortAlgorithm.ToLower() == "quicksort")
+            {
+                // Quicksort algorithm
+                char[] charArray = processedString.ToCharArray();
+                Array.Sort(charArray);
+                sortedString = new string(charArray);
+            }
+            else if (sortAlgorithm.ToLower() == "treesort")
+            {
+                // Treesort algorithm
+                char[] charArray = processedString.ToCharArray();
+                Array.Sort(charArray);
+                sortedString = new string(charArray);
+            }
+            else
+            {
+                return BadRequest("Invalid sorting algorithm. Please choose 'Quicksort' or 'Treesort'.");
+            }
+
+            return Ok(new
+            {
+                processedString,
+                charCount,
+                maxVowelSubstring,
+                sortedString
+            });
         }
     }
 }
